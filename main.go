1cfe5dc9ec31ab6df033232d392da3a520597ace
Hau Nguyen <hauvipapro@gmail.com>
2023-08-17 09:37:38 +0700

    bump: go-github v54

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/google/go-github/v53/github"
+	"github.com/google/go-github/v54/github"
 	"golang.org/x/oauth2"
 	"golang.org/x/sync/errgroup"
 
