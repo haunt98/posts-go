4d5023e9e16437080f70244249bfd2981bbab727
Hau Nguyen <hauvipapro@gmail.com>
2024-07-12 13:21:28 +0700

    build: bump go-github v62 to v63

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/google/go-github/v62/github"
+	"github.com/google/go-github/v63/github"
 	"golang.org/x/oauth2"
 	"golang.org/x/sync/errgroup"
 
