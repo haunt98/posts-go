9e2afe7d88a47c4f6f5a3d0f90cea695b541bc23
Hau Nguyen <hauvipapro@gmail.com>
2025-11-09 01:39:59 +0900

    build: bump go deps
    - github.com/google/go-github/v78 v78.0.0

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/google/go-github/v77/github"
+	"github.com/google/go-github/v78/github"
 	"golang.org/x/oauth2"
 	"golang.org/x/sync/errgroup"
 
